1cc1086a8890a16063f65cbabad7047359c6cb2c
lbbgit <libingbingxx@126.com>
2018-02-05 22:18:28 +0800

    geturl saveFile(codecs)

--- temp2.py
+++ temp2.py
@@ -1,9 +1,49 @@
 import urllib
 import urllib2
 
-values = {"username": "1016903103@qq.com", "password": "XXXX"}
+url = 'http://www.server.com/login'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+values = {'username': 'cqc', 'password': 'XXXX'}
+headers = {'User-Agent': user_agent}
+headers['Referer']='http://www.zhihu.com/articles'
 data = urllib.urlencode(values)
-url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
-request = urllib2.Request(url, data)
+request = urllib2.Request(url, data, headers)
 response = urllib2.urlopen(request)
-print response.read()
+page = response.read()
+
+
+
+#proxy
+enable_proxy = False  # True
+proxy_handler = urllib2.ProxyHandler({"http" : 'http://some-proxy.com:8080'})
+response = urllib2.urlopen('http://www.baidu.com', timeout=10)
+#response= urllib2.urlopen('http://www.baidu.com',timeout=10)
+#response= urllib2.urlopen('http://www.baidu.com',data, 10)
+null_proxy_handler = urllib2.ProxyHandler({})
+if enable_proxy:
+    opener = urllib2.build_opener(proxy_handler)
+else:
+    opener = urllib2.build_opener(null_proxy_handler)
+proxy_openstr=urllib2.install_opener(opener)
+
+print page
+print proxy_openstr
+
+
+
+
+
+# use cookie
+import urllib2
+import cookielib
+#声明一个CookieJar对象实例来保存cookie
+cookie = cookielib.CookieJar()
+#利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
+handler=urllib2.HTTPCookieProcessor(cookie)
+#通过handler来构建opener
+opener = urllib2.build_opener(handler)
+#此处的open方法同urllib2的urlopen方法，也可以传入request
+response = opener.open('http://www.baidu.com')
+for item in cookie:
+    print 'Name = '+item.name
+    print 'Value = '+item.value
--- temp3.py
+++ temp3.py
@@ -19,3 +19,27 @@ data3=response.read().decode('UTF-8')
 print ('result:',data==data2,data==data3)
 
 print  len(data),len(data2),len(data3)
+
+
+
+#urllib ,urllib2
+import urllib
+import urllib2
+#等效于如下
+values = {}
+values['username'] = "1016903103@qq.com"
+values['password'] = "XXXX"
+#等效于如上
+values = {"username": "1016903103@qq.com", "password": "XXXX"}
+data = urllib.urlencode(values)
+url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
+request = urllib2.Request(url, data)
+response = urllib2.urlopen(request)
+print response.read()
+
+
+#以上是Post，这里是Get
+values = {"username": "1016903103@qq.com", "password": "XXXX"}
+data = urllib.urlencode(values)
+geturl = "http://passport.csdn.net/account/login" + "?"+data
+
