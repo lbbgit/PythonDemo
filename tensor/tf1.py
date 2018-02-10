7fa51b36489c5785a77fa2e72840a7d0f73d5bb1
lbbgit <libingbingxx@126.com>
2018-02-10 22:39:44 +0800

    .

---
+++ tensor/tf1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+
+graph = tf.Graph()
+with graph.as_default():
+    foo = tf.Variable(3, name='foo')
+    bar = tf.Variable(2, name='bar')
+    result = foo + bar
+    initialize = tf.global_variables_initializer()
+
+print(result)  # Tensor("add:0", shape=(), dtype=int32)
+
+
+# 定义一个session，并进行真正的计算。
+with tf.Session(graph=graph) as sess:
+    sess.run(initialize)
+    res = sess.run(result)
+print(res)  # 5
